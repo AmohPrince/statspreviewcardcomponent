e9bfdb8bd50b57309cd5a7a496a0224feb5c0730
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-05-24 12:18:31 +0300

    Just beat my 75 mins record

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import image from "./images/image-header-desktop.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App flex__container">
+      <div className="App__left">
+        <p className="Main__text">
+          Get <span className="insights">insights</span> that help your business
+          grow.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="sub__text">
+          Discover the benefits of data analytics and make better decisions
+          regarding revenue, customer experience, and overall efficiency.
+        </p>
+        <div className="App__left-bottom flex__container">
+          <div className="first-section">
+            <p className="number">10K+</p>
+            <p className="description">Companies</p>
+          </div>
+          <div className="second-section">
+            <p className="number">314</p>
+            <p className="description">Templates</p>
+          </div>
+          <div className="first-section">
+            <p className="number">12M+</p>
+            <p className="description">Queries</p>
+          </div>
+        </div>
+      </div>
+      <div className="App__right">
+        <img src={image} alt="People on computers" />
+      </div>
     </div>
   );
 }
